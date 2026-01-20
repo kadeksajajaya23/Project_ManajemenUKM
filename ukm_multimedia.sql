98654a0569bbd488348c609e10fdbd5844e7a6b2
Saja Jaya <kadeksajajaya23@gmail.com>
2026-01-20 16:31:40 +0800

    create database, create table anggota, and add mysql connector java

---
+++ ukm_multimedia.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE ukm_multimedia;
+USE ukm_multimedia;
+
+-- Tabel Anggota
+CREATE TABLE IF NOT EXISTS anggota (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nomor_anggota VARCHAR(20) NOT NULL UNIQUE,
+    nim VARCHAR(15) NOT NULL UNIQUE,
+    nama VARCHAR(100) NOT NULL,
+    kelas VARCHAR(10) NOT NULL,
+    divisi VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    foto_path VARCHAR(255),
+    role VARCHAR(20) DEFAULT 'Anggota'
+);
+
