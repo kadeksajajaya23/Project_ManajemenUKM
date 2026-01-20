a562c0a488986aeb561f66857e39f9f62c7cd9b1
Saja Jaya <kadeksajajaya23@gmail.com>
2026-01-20 16:39:15 +0800

    membuat database connection dan juga menampilkan pesan error

---
+++ Koneksi.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Koneksi {
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
+
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ukm_multimedia", "root", "");
+            
+        } catch (ClassNotFoundException e) {
+            System.err.println("ERROR: Driver MySQL tidak ditemukan!");
+            System.err.println("Pastikan file .jar sudah dimasukkan ke Library.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("ERROR: Gagal Konek ke Database!");
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
