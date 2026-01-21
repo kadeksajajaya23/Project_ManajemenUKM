b15e41e7699854b4c538efeff69dc7b084b146c9
kayuputih245-lab <kayuputih245@gmail.com>
2026-01-21 16:18:58 +0800

    membuat register dan login field

--- LoginFrame.java
+++ LoginFrame.java
@@ -43,4 +43,43 @@ public class LoginFrame extends JFrame {
         lblTitle.setFont(Style.FONT_HEADER);
         lblTitle.setForeground(Style.COLOR_ACCENT);
 
-    }
+        // === REGISTER FIELD ===
+        txtNim = new JTextField();
+        txtNama = new JTextField();
+
+        cmbKelas = new JComboBox<>(new String[]{"A", "B1", "B2"});
+        cmbDivisi = new JComboBox<>(new String[]{
+                "3D Modelling",
+                "Graphic Design",
+                "Audio & Video Editing"
+        });
+
+        cmbJabatan = new JComboBox<>(new String[]{
+                "Sekretaris",
+                "Bendahara",
+                "Koordinator",
+                "Anggota Inti"
+        });
+        cmbJabatan.setBorder(BorderFactory.createTitledBorder("Pilih Jabatan"));
+
+        btnPilihFoto = new JButton("Upload Foto Profil");
+        Style.styleButton(btnPilihFoto);
+        btnPilihFoto.setBackground(Color.GRAY);
+
+        lblFotoPreview = new JLabel("", SwingConstants.CENTER);
+        lblFotoPreview.setForeground(Color.WHITE);
+
+        // === LOGIN FIELD ===
+        txtUser = new JTextField();
+        txtPass = new JPasswordField();
+
+        btnAction = new JButton();
+        Style.styleButton(btnAction);
+
+        btnSwitch = new JButton();
+        btnSwitch.setContentAreaFilled(false);
+        btnSwitch.setBorderPainted(false);
+        btnSwitch.setForeground(Color.WHITE);
+
+    
+}
