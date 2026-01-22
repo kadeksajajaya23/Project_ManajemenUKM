97f729a40df31a5bd2d2c325aafc8e017799131a
Saja Jaya <kadeksajajaya23@gmail.com>
2026-01-22 20:26:56 +0800

    update sql

--- ukm_multimedia.sql
+++ ukm_multimedia.sql
@@ -1,22 +1,22 @@
 CREATE DATABASE ukm_multimedia;
 USE ukm_multimedia;
 
--- Tabel Anggota
-CREATE TABLE IF NOT EXISTS anggota (
+CREATE TABLE anggota (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nomor_anggota VARCHAR(20) NOT NULL UNIQUE,
+    nomor_anggota VARCHAR(20),
     nim VARCHAR(15) NOT NULL UNIQUE,
     nama VARCHAR(100) NOT NULL,
-    kelas VARCHAR(10) NOT NULL,
-    divisi VARCHAR(50) NOT NULL,
+    kelas VARCHAR(10),
+    divisi VARCHAR(50),
+    jabatan VARCHAR(50),
     username VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
     foto_path VARCHAR(255),
-    role VARCHAR(20) DEFAULT 'Anggota'
+    role VARCHAR(20) DEFAULT 'Anggota',
+    status VARCHAR(20) DEFAULT 'Active'  -- KOLOM BARU UNTUK VALIDASI
 );
 
--- Tabel Portofolio
-CREATE TABLE IF NOT EXISTS portofolio (
+CREATE TABLE portofolio (
     id INT AUTO_INCREMENT PRIMARY KEY,
     anggota_id INT,
     judul_karya VARCHAR(100),
@@ -24,6 +24,6 @@ CREATE TABLE IF NOT EXISTS portofolio (
     FOREIGN KEY (anggota_id) REFERENCES anggota(id) ON DELETE CASCADE
 );
 
--- AKUN SUPER ADMIN 
-INSERT INTO anggota (nomor_anggota, nim, nama, kelas, divisi, username, password, role) 
-VALUES ('MM-INTI-001', '00000', 'Super Admin', 'Inti', 'Pengurus', 'Saja_Jaya', 'Multimedia26', 'Admin');
+
+INSERT INTO anggota (nomor_anggota, nim, nama, kelas, divisi, jabatan, username, password, role, status) 
+VALUES ('MM-KETUA-01', '00000', 'Super Admin', NULL, NULL, 'Ketua Umum', 'Saja_Jaya', 'Multimedia26', 'ketua', 'Active');
