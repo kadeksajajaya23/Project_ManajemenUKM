13f12ae05fc685696f22bb074519e8658252179e
narendrap54 <narendrajaya13@gmail.com>
2026-01-23 13:40:14 +0800

    update koneksi database

--- Koneksi.java
+++ Koneksi.java
@@ -6,8 +6,11 @@ public class Koneksi {
     public static Connection getConnection() {
         Connection conn = null;
         try {
+            // 1. PENTING: Paksa load driver MySQL
+            // Ini akan memancing error yang lebih jelas jika library tidak ketemu
             Class.forName("com.mysql.cj.jdbc.Driver"); 
 
+            // 2. Koneksi ke Database
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ukm_multimedia", "root", "");
             
         } catch (ClassNotFoundException e) {
